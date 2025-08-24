b01de5a6371bc2fe00d0e23c01b24e76e2ac6194
jeancgarciaq <jcvictory@hotmail.com>
2025-08-24 18:57:28 -0400

    feat: use the repository to generate collections from the views

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SnippetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,18 +14,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController 
 {
 	#[Route('/', name: 'home')]
-	public function index(): Response
+	public function index(SnippetRepository $snippetRepository): Response
 	{
+		$snippets = $snippetRepository->findAll();
+
 		return $this->render('pages/home.html.twig', [
 			'title' => 'Home Page',
+			'snippets' => $snippets,
 		]);
 	}
 
-	#[Route('/item', name: 'item')]
-    public function item(): Response
+	#[Route('/item/{id}', name: 'item')]
+    public function item(int $id, SnippetRepository $snippetRepository): Response
     {
+    	$snippet = $snippetRepository->find($id);
+
+    	if (!$snippet) 
+    	{
+    		 throw $this->createNotFoundException('Snippet not found');
+    	}
+
         return $this->render('pages/item.html.twig', [
-            'title' => 'Item Page',
+            'title' => $snippet->getTitle(),
+            'snippet' => $snippet,
         ]);
     }
 
